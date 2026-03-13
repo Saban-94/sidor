e786a5da56ef92ad81a3bd6f597b3aad0c9321a4
Saban-94 <hsaban2025@gmail.com>
2026-03-13 07:28:27 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,18 +3,25 @@ const withPWA = require('next-pwa')({
   dest: 'public',         // מיקום קבצי ה-Service Worker
   register: true,         // רישום אוטומטי של ה-SW
   skipWaiting: true,      // עדכון גרסה מיידי כשיש שינוי ב-PWA
-  disable: process.env.NODE_ENV === 'development', // ביטול PWA בסביבת פיתוח כדי למנוע קאש מעצבן
+  disable: process.env.NODE_ENV === 'development', // ביטול PWA בסביבת פיתוח
 });
 
 const nextConfig = {
   reactStrictMode: true,
-  // הגדרת משתני סביבה שיעברו גם לצד הלקוח אם צריך
   env: {
     FIREBASE_DATABASE_URL: "https://whatsapp-8ffd1-default-rtdb.europe-west1.firebasedatabase.app/",
   },
-  // אופציונלי: אם אתה משתמש בתמונות ממקור חיצוני (כמו פרופילי וואטסאפ)
   images: {
-    domains: ['pps.whatsapp.net'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pps.whatsapp.net',
+      },
+    ],
+  },
+  // השורה הקריטית לפתרון השגיאה בגרסה 16 ומעלה
+  experimental: {
+    turbopack: {},
   },
 };
 
