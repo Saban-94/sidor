7511213b7facacf0c93456e11d3e0c59028eff0d
Saban-94 <hsaban2025@gmail.com>
2026-03-13 07:57:31 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 // /postcss.config.js
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    '@tailwindcss/postcss': {}, // שימוש בחבילה החדשה שהוספנו
+    'autoprefixer': {},
   },
 }
