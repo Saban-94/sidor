fa5da4e39261dbdedf455491ddd1f09fff0137be
Saban-94 <hsaban2025@gmail.com>
2026-03-13 07:30:36 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,21 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
-  dest: 'public',         // מיקום קבצי ה-Service Worker
-  register: true,         // רישום אוטומטי של ה-SW
-  skipWaiting: true,      // עדכון גרסה מיידי כשיש שינוי ב-PWA
-  disable: process.env.NODE_ENV === 'development', // ביטול PWA בסביבת פיתוח
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
 });
 
 const nextConfig = {
   reactStrictMode: true,
+  
+  // פתרון השגיאה: הגדרת אובייקט turbopack ריק ברמה הראשית
+  turbopack: {}, 
+
   env: {
     FIREBASE_DATABASE_URL: "https://whatsapp-8ffd1-default-rtdb.europe-west1.firebasedatabase.app/",
   },
+  
   images: {
     remotePatterns: [
       {
@@ -18,10 +23,6 @@ const nextConfig = {
         hostname: 'pps.whatsapp.net',
       },
     ],
-  },
-  // השורה הקריטית לפתרון השגיאה בגרסה 16 ומעלה
-  experimental: {
-    turbopack: {},
   },
 };
 
