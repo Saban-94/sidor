a2e1afa69f45f02da584e8368e59746a4b1e842f
Saban-94 <hsaban2025@gmail.com>
2026-03-24 04:12:39 +0200

    Update PostCSS configuration for Tailwind CSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
-// /postcss.config.js
 module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {}, // שימוש בחבילה החדשה שהוספנו
-    'autoprefixer': {},
+    tailwindcss: {},
+    autoprefixer: {},
   },
-}
+};
